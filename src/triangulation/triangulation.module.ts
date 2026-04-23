cee23cc9665a18a6742f3257062a97a60bddf66c
Adeline <adelinetuyizere333@gmail.com>
2026-04-23 23:37:33 +0200

    chore: add LocationModule to TriangulationModule imports

--- src/triangulation/triangulation.module.ts
+++ src/triangulation/triangulation.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TriangulationController } from './triangulation.controller';
 import { TriangulationService } from './triangulation.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module'
+import { LocationModule } from '../location/location.module';
+
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, LocationModule],
   controllers: [TriangulationController],
   providers: [TriangulationService]
 })
