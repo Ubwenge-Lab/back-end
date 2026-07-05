aebf821f1e79e91999b3820895574a78bab7cbff
Robert <r.niyonkuru@alustudent.com>
2026-07-05 22:32:20 +0200

    fix:ran tests and fixed failing risks and regression safety

--- src/doctors/availability/availability.controller.spec.ts
+++ src/doctors/availability/availability.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AvailabilityController } from './availability.controller';
+import { AvailabilityService } from './availability.service';
 
 describe('AvailabilityController', () => {
   let controller: AvailabilityController;
@@ -7,6 +8,12 @@ describe('AvailabilityController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AvailabilityController],
+      providers: [
+        {
+          provide: AvailabilityService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<AvailabilityController>(AvailabilityController);
